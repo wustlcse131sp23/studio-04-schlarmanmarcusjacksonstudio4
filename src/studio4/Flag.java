05e098cd3f73993d910e600e834aa0d1927df439
schla <schla@DESKTOP-9IV1OT2.dhcp.wustl.edu>
2023-02-23 15:26:24 -0600

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,12 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(Color.LIGHT_GRAY);
+		StdDraw.filledRectangle(0.5,0.5,0.4,0.2);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5,0.5,0.15,0.025);
+		StdDraw.filledRectangle(0.5,0.5,0.025,0.15);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledEllipse(0.54, 0.54, 0.02, 0.02);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,7 +18,25 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner in = new Scanner(f); //making Scanner with a File	
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		if (shapeType.equals("rectangle")) {
+			if (isFilled==true) {
+				StdDraw.filledRectangle(parameterOne,parameterTwo,parameterThree,parameterFour);
+			}else {
+				StdDraw.rectangle(parameterOne,parameterTwo,parameterThree,parameterFour);
+			}
+		}
 	}
 }
+
